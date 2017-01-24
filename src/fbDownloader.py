dbf08d57a0be66a37be03c2960c37fc8481e9dd7
Micro <maglars2@gmail.com>
2017-01-24 17:28:06 +0100

    Working fbDownloader with new set of (working) test data

--- src/fbDownloader.py
+++ src/fbDownloader.py
@@ -1,14 +1,14 @@
 import json
 import os
+import os.path
 import requests
 import time
 
-PROJECT_NAME = 'temp'
 MAX_RETRIES = 4
 current_milli_time = lambda: int(round(time.time() * 1000))
 
 def getURLContent(url):
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"}
     isDownloaded = False
     retryWait = 1
     retriesLeft = MAX_RETRIES
@@ -28,31 +28,76 @@ def getURLContent(url):
     if isDownloaded:
         return response.content
     else:
-        raise Exception('Data could not be downloaded from '+url+'. Check that you are online and authorized!')
+        raise Exception("Data could not be downloaded from "+url+". Check that you are online and authorized!")
 
 def saveData(datadump, filename):
     with open(filename+".json", "w+") as f:
         f.write(datadump)
 
-def makePath():
-    path = os.path.join(os.getcwd(), PROJECT_NAME)
+def makePath(directory):
+    path = os.path.join(os.getcwd(), directory)
     if(not os.path.isdir(path)):
         os.mkdir(path)
         
     return path
 
+def getExistingProjectFiles(projectName):
+    path = os.path.join(os.getcwd(), projectName)
+    return [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+
+def getNewestDownloadedMessageId(projectDir):
+    projectFiles = getExistingProjectFiles(projectDir)
+    if len(projectFiles) == 0:
+        return None
+    
+    projectFiles.sort();
+
+    latestFileName = projectFiles[-1]
+    latestJson = json.load(open(latestFileName))
+
+    if "comments" in latestJson:
+        latestJson = latestJson["comments"]
+        
+    messages = latestJson["data"]
+
+    if len(messages) == 0:
+        return None
+    else:
+        return messages[-1]['id']
+
+def containsNewestMessage(messages, target):
+    for message in messages:
+        if message["id"] == target:
+            return True
+    return False
+
+def getMessagesNewerThan(messages, target):
+    newMessages = []
+    for message in reversed(messages):
+        if message["id"] == target:
+            break
+        else:
+            newMessages.append(message)
+    return newMessages
+
 def main():
 
+    PROJECT_NAME = "<YOUR_PROJECT>"
     START_URL = "<YOUR_URL>"
     url = START_URL
-    path = makePath()
-    print "Downloading from "+str(url)
+    path = makePath(PROJECT_NAME)
+    newestMessageId = getNewestDownloadedMessageId(PROJECT_NAME);
 
+    if newestMessageId is not None:
+        print "Downloading until last message: " + newestMessageId+"\n\n"
+    
+    print "Downloading from "+str(url)+"\n"
+    
     while True:
         # 1) Download data
         try:
             jsonData = getURLContent(url)
-            print "Finished downloading data from "+str(url)
+            print "Finished downloading data from "+str(url)+"\n"
         except Exception as e:
             print e
             break
@@ -67,17 +112,41 @@ def main():
             break
         
         # 3) Save to file
-        createTime = data[-1]["created_time"]
-        filename = createTime.replace("-", "").replace(":", "").replace("+", "")
+        messages = data["data"]
+        isLastFile = len(messages) == 0
+        shouldSaveData = True
 
-        saveData(jsonData, os.path.join(path, filename))
+        if isLastFile:
+            filename = "00000000T000000-default"
+        else:
+            # 3.1) Check if we all or a subset of the messages are new
+            if containsNewestMessage(messages, newestMessageId):
+                isLastFile = True
+                newMessages = getMessagesNewerThan(messages, newestMessageId)
+                print "Found latest downloaded message, extracted "+str(len(newMessages))+" newer messages..."
+
+                if len(newMessages) > 0:
+                    data["data"] = newMessages # Override list of messages with only new ones
+                    createTime = newMessages[-1]["created_time"]
+                    filename = createTime.replace("-", "").replace(":", "").replace("+", "")
+                else:
+                    shouldSaveData = False
+            else:
+                createTime = messages[-1]["created_time"]
+                filename = createTime.replace("-", "").replace(":", "").replace("+", "")
+
+        if shouldSaveData:
+            saveData(jsonData, os.path.join(path, filename))
+
+        if isLastFile:
+            break
 
         # 4) Get next URL to download
         if "paging" in data:
             url = data["paging"]["next"] # If "paging" does not exist, you have reached the end
         else:
             break
-        print "Next url to use: "+str(url)
+        print "Next url to use: "+str(url)+"\n"
 
         # 5) Sleep at least 1 second to avoid getting blocked
         time.sleep(1)
